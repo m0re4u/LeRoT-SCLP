b151f464c7c1a8e2bc1463fa60343a47f6186ce4
Maarten Boon <m_l_boon@hotmail.com>
2016-06-02 12:29:30 +0200

    bug fix, for uneven index 1 was in the list twice and not the 0 index once

--- lerot/perturbation/formPairs.py
+++ lerot/perturbation/formPairs.py
@@ -2,8 +2,8 @@
 This function forms pairs of indices according to 
 the raman et al. 2013 paper for 3PR
 
-Takes length
-returns paired indices list 
+@Params: length
+@returns: paired indices list 
 """
 
 def formPairs(length):
@@ -12,9 +12,9 @@ def formPairs(length):
 	if length % 2 == 0:
 		for i in xrange(0,length-1,2):
 			paired_indices.append([i, i+1])
-    # Uneven, take the first number on it's own pair the rest
+    # Uneven, take the first number on it's own pair the returns
 	else:
-		paired_indices = [[1]]
+		paired_indices = [[0]]
 		for i in xrange(1,length-1,2):
 			paired_indices.append([i, i+1]) 
 	return paired_indices
