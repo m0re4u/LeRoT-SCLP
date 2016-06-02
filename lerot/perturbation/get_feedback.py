aa9d82831827d6e6a9ed3158eb0e5475c2f863e8
Michiel van der Meer <michielmeer@live.nl>
2016-06-02 14:05:45 +0200

    implemented get_feedback :boom:

---
+++ lerot/perturbation/get_feedback.py
@@ -0,0 +1,19 @@
+"""
+Get a new ranking, swapped according to user clicks
+"""
+
+
+def get_feedback(current_ranking, clicks, pairs):
+    feedback_ranking = current_ranking
+    for pair in pairs:
+        if len(pair) == 1:
+            continue
+        else:
+            # lower and upper are indices of elements in current_ranking
+            lower, upper = pair
+        # if lower is clicked and upper is not, swap the two
+        if lower in clicks and upper not in clicks:
+            feedback_ranking[upper], feedback_ranking[lower] = \
+                feedback_ranking[lower], feedback_ranking[upper]
+
+        return feedback_ranking
