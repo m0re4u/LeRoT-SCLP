0a5c6a4abe9700cff69769e68574a916637a77f8
Maarten Boon <m_l_boon@hotmail.com>
2016-06-02 13:35:15 +0200

    fixed formPairs to randomly begin with a singular starter

--- lerot/perturbation/formPairs.py
+++ lerot/perturbation/formPairs.py
@@ -5,16 +5,29 @@ the raman et al. 2013 paper for 3PR
 @Params: length
 @returns: paired indices list 
 """
+import random as rnd
 
 def formPairs(length):
-	paired_indices = []
-	# If even, go from the start and form pairs
-	if length % 2 == 0:
-		for i in xrange(0,length-1,2):
-			paired_indices.append([i, i+1])
-    # Uneven, take the first number on it's own pair the returns
-	else:
-		paired_indices = [[0]]
-		for i in xrange(1,length-1,2):
-			paired_indices.append([i, i+1]) 
-	return paired_indices
+    # Init flag for single item that needs addition to paired_indeces
+    has_one = True
+    if length % 2 == 0:
+         has_one = False
+
+    # p=0.5 for not using singular starter
+    if rnd.randint(0,1):
+        paired_indices = []
+    
+    # Else, assign singular starter, swap flag
+    else:
+        paired_indices = [[0]]
+        has_one = not has_one
+
+    # Gather all pairs of 2 that have no item in indices yet    
+    for i in xrange(len(paired_indices),length-1,2):
+        paired_indices.append([i, i+1])
+    
+    # Add last index if it hasn't been added yet
+    if has_one:
+        paired_indices.append([length-1])
+
+    return paired_indices
