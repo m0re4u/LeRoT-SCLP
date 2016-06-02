2fccdc5c5f8f3d28ae8e880ceec3fcfede079c14
Maarten Boon <m_l_boon@hotmail.com>
2016-06-02 11:20:40 +0200

    added inline comments for formPairs

--- lerot/perturbation/formPairs.py
+++ lerot/perturbation/formPairs.py
@@ -8,9 +8,11 @@ returns paired indices list
 
 def formPairs(length):
 	paired_indices = []
+	# If even, go from the start and form pairs
 	if length % 2 == 0:
 		for i in xrange(0,length-1,2):
 			paired_indices.append([i, i+1])
+    # Uneven, take the first number on it's own pair the rest
 	else:
 		paired_indices = [[1]]
 		for i in xrange(1,length-1,2):
