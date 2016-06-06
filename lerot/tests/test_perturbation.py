33d0f80e29fb218efa21d7252f0828bf775632c4
Martin van Harmelen <Martin@vanharmelen.com>
2016-06-06 12:23:35 +0200

    Added an "empty" test file

---
+++ lerot/tests/test_perturbation.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import unittest
+
+sys.path.insert(0, os.path.abspath('..'))
+
+
+class TestQuery(unittest.TestCase):
+    def test_join(self):
+        """This is an example test function"""
+        self.assertEquals("Some string", ' '.join(["Some", "string"]))
+
+
+if __name__ == '__main__':
+    unittest.main()
