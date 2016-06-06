16419680c25f1d31bdfa5306f3e3c50f282d1d52
Martin van Harmelen <Martin@vanharmelen.com>
2016-06-06 14:19:47 +0200

    Added setUp for TestPerturbation that sets random seed

--- lerot/tests/test_perturbation.py
+++ lerot/tests/test_perturbation.py
@@ -1,7 +1,12 @@
 import unittest
+import random
 
 
 class TestPerturbation(unittest.TestCase):
+    def setUp(self):
+        # Seed the random module to be able to predict the outcome of tests
+        random.seed(42)
+
     def test_fixed_perturbation(self):
         """
         Test whether perturbation with a fixed probability works as expected
