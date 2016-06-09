20ecf015f4cda34c32795a9baf4d68a393b0e7f3
Maarten Boon <m_l_boon@hotmail.com>
2016-06-09 16:23:02 +0200

    added merge_folds to merge folds of a dataset

---
+++ VisualEval/merge_folds.py
@@ -0,0 +1,29 @@
+import os
+import os.path
+
+def merge_folds(data_dir):
+"""
+Given data directory (which holds folds)
+merges all files within these folds and returns them to
+data_dir/Merged_Folds/
+"""
+    all_files = []
+    roots = []
+    for (root, dirs, files) in os.walk(data_dir):
+        all_files += files
+        roots.append(root)
+    print(roots)
+    dir_name = roots[0] + "/Merged_Folds/"
+    os.mkdir(dir_name)
+    for file in files:
+        file_data = []
+        for root in roots:
+            try:
+                rf = open(root +'/' + file, 'r')
+                with open(dir_name + file, 'a') as wf:
+                    for line in rf:
+                        wf.write(line)
+                rf.close()
+            except IOError as e:
+                continue
+    return dir_name
