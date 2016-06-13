4908d4d4f6488fba59409d206fac086ffa5a65e6
Michiel van der Meer <michielmeer@live.nl>
2016-06-13 14:37:08 +0200

    added average of yaml evaluation

--- VisualEval/visualize_json.py
+++ VisualEval/visualize_json.py
@@ -1,10 +1,13 @@
-import json
 import argparse
+import numpy
+import gzip
+import json
 import os
+import yaml
 from pprint import pprint
 from multiple_plots import multiple_plots
 
-def visualize_json(evaluation, folder, x, y, z, max_bound, title):
+def visualize_json(evaluation, folder, x, y, z, max_bound, title, mean=False):
     """
     ...
     """
@@ -15,9 +18,11 @@ def visualize_json(evaluation, folder, x, y, z, max_bound, title):
     z_labels = []
 
     for filename in os.listdir(folder):
-        with open(os.path.join(folder, filename), 'r') as file:
-            data = json.load(file)
-
+        data = None
+        with gzip.open(os.path.join(folder, filename), 'r') as data_file:
+            print("Reading " + str(filename))
+            data = yaml.load(data_file.read())
+        
         z_labels.append(z + " " + filename.split("@")[0])
 
         if evaluation == "offline":
@@ -31,7 +36,14 @@ def visualize_json(evaluation, folder, x, y, z, max_bound, title):
         x_data.append(list(range(1,len(data)+1)))
         y_pos.append(data[-1])
 
-    multiple_plots(x_data, y_data, y_pos, max_bound, title, x, y, z_labels)
+    if mean:
+        x_mean = numpy.mean(x_data, axis=0)
+        y_mean = numpy.mean(y_data, axis=0)
+        multiple_plots([x_mean], [y_mean], y_pos, max_bound, title, x, y, z_labels)
+    else:
+        multiple_plots(x_data, y_data, y_pos, max_bound, title, x, y, z_labels)
+        
+
 
 
 if __name__ == "__main__":
@@ -47,7 +59,8 @@ if __name__ == "__main__":
     parser.add_argument("-max", "--max_bound",
                         help="maximum number for x-axis", type=int)
     parser.add_argument("-title", help="title for plot")
+    parser.add_argument("-mean", help="take the average of all evals", action='store_true')
     args = parser.parse_args()
 
     visualize_json(args.measure, args.folder_name, args.x_label, args.y_label,
-        args.z_label, args.max_bound, args.title)
+        args.z_label, args.max_bound, args.title, args.mean)
