c820b0ccea8264ba4299bccabab189cf5a605171
Verna Dankers <verna_dankers@hotmail.com>
2016-06-15 10:24:32 +0200

    Updated yaml file

--- visual_eval/visualize_yaml.py
+++ visual_eval/visualize_yaml.py
@@ -18,37 +18,59 @@ def visualize_json(evaluation, folder, x, y, z, max_bound, title, mean=False):
     y_pos = []
     z_labels = []
 
+    if evaluation == "offline":
+        evaluation = "offline_test"
+
     for filename in os.listdir(folder):
+        x_data, y_data, y_pos, z_labels = open_file(evaluation, filename,
+            folder, x_data, y, y_data, y_pos, z, z_labels, mean)
+
+    # if mean:
+    #     x_mean = numpy.mean(x_data, axis=0)
+    #     y_mean = numpy.mean(y_data, axis=0)
+    #     multiple_plots([x_mean], [y_mean], y_pos, max_bound, title, x,
+    #                    y, z_labels)
+    # else:
+    multiple_plots(x_data, y_data, y_pos, max_bound, title, x, y, z_labels)
+
+def open_file(evaluation, filename, folder, x_data, y, y_data, y_pos, z, z_labels, mean):
+
+    if os.path.isdir(os.path.join(folder,filename)):
+        new_folder = os.path.join(folder,filename)
+        temp_x = []
+        temp_y = []
+        temp_y_pos = []
+        temp_z = []
+        for file in os.listdir(new_folder):
+            temp_x, temp_y, temp_y_pos, temp_z = open_file(evaluation, file,
+                new_folder, temp_x, y, temp_y, temp_y_pos, z, temp_z, mean)
+        if mean:
+            x_data.append(numpy.mean(temp_x, axis=0))
+            y_data.append(numpy.mean(temp_y, axis=0))
+            y_pos.append(numpy.mean(temp_y_pos, axis=0))
+            z_labels.append(filename.split('@')[0])
+    else:
         data = None
         _, ext = os.path.splitext(filename)
         # Exclude some standard files that are normally in the folder generated
         # by an experiment(particularly LearningExperiment)
-        if ext != '.gz' or filename.startswith('_'):
-            continue
-        with gzip.open(os.path.join(folder, filename), 'r') as data_file:
-            print("Reading " + str(filename))
-            data = yaml.load(data_file.read())
+        if ext == '.gz' and not filename.startswith('_'):
 
-        z_labels.append(z + " " + filename.split("@")[0])
+            with gzip.open(os.path.join(folder, filename), 'r') as data_file:
+                print("Reading " + str(filename))
+                data = yaml.load(data_file.read())
 
-        if evaluation == "offline":
-            evaluation = "offline_test"
+            z_labels.append(z + " " + filename.split("@")[0])
 
-        for measure in data:
-            if evaluation in measure and y in measure:
-                data = data[measure]
+            for measure in data:
+                if evaluation in measure and y in measure:
+                   data = data[measure]
 
-        y_data.append(data)
-        x_data.append(list(range(1, len(data)+1)))
-        y_pos.append(data[-1])
+            y_data.append(data)
+            x_data.append(list(range(1, len(data)+1)))
+            y_pos.append(data[-1])
 
-    if mean:
-        x_mean = numpy.mean(x_data, axis=0)
-        y_mean = numpy.mean(y_data, axis=0)
-        multiple_plots([x_mean], [y_mean], y_pos, max_bound, title, x,
-                       y, z_labels)
-    else:
-        multiple_plots(x_data, y_data, y_pos, max_bound, title, x, y, z_labels)
+    return (x_data, y_data, y_pos, z_labels)
 
 
 if __name__ == "__main__":
@@ -65,7 +87,7 @@ if __name__ == "__main__":
                         help="maximum number for x-axis", type=int)
     parser.add_argument("-title", help="title for plot")
     parser.add_argument("-mean", help="take the average of all evals",
-                        action='store_true')
+                        type=bool)
     args = parser.parse_args()
 
     visualize_json(args.measure, args.folder_name, args.x_label, args.y_label,
