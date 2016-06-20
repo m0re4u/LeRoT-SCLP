1a31511e47a1c37b5cbd5c5fb909b4019599e919
Martin van Harmelen <Martin@vanharmelen.com>
2016-06-20 13:38:40 +0200

    pep8

--- visual_eval/visualize_yaml.py
+++ visual_eval/visualize_yaml.py
@@ -7,8 +7,9 @@ import os
 import yaml
 from multiple_plots import multiple_plots
 
+
 def visualize_yaml(evaluation, folder, x, y, max_bound, ymin=0, ymax=1,
-    title="plot", run_mean=1):
+                   title="plot", run_mean=1):
     """
     Visualize the specified evaluation from the folder given.
     Setting the mean flag will assume each file is one run, and take the
@@ -24,8 +25,9 @@ def visualize_yaml(evaluation, folder, x, y, max_bound, ymin=0, ymax=1,
         evaluation = "offline_test"
 
     for filename in os.listdir(folder):
-        x_data, y_data, y_pos, z_labels = open_file(evaluation, filename,
-            folder, x_data, y, y_data, y_pos, z_labels)
+        x_data, y_data, y_pos, z_labels = open_file(
+            evaluation, filename, folder, x_data, y, y_data, y_pos, z_labels
+        )
 
     for i in range(len(y_data)):
         x_data[i] = x_data[i][:-run_mean]
@@ -36,23 +38,28 @@ def visualize_yaml(evaluation, folder, x, y, max_bound, ymin=0, ymax=1,
             ymax = y_data[i][-1]
         y_pos.append(y_data[i][-1])
 
-    multiple_plots(x_data, y_data, y_pos, max_bound-run_mean, ymin, ymax, title,
-        x, y, z_labels)
+    multiple_plots(x_data, y_data, y_pos, max_bound-run_mean, ymin, ymax,
+                   title, x, y, z_labels)
+
 
 def running_mean(x, N):
     return np.convolve(x, np.ones((N,))/N)[(N-1):]
 
-def open_file(evaluation, filename, folder, x_data, y, y_data, y_pos, z_labels):
 
-    if os.path.isdir(os.path.join(folder,filename)):
-        new_folder = os.path.join(folder,filename)
+def open_file(evaluation, filename, folder, x_data, y, y_data, y_pos,
+              z_labels):
+
+    if os.path.isdir(os.path.join(folder, filename)):
+        new_folder = os.path.join(folder, filename)
         temp_x = []
         temp_y = []
         temp_y_pos = []
         temp_z = []
         for file in os.listdir(new_folder):
-            temp_x, temp_y, temp_y_pos, temp_z = open_file(evaluation, file,
-                new_folder, temp_x, y, temp_y, temp_y_pos, temp_z)
+            temp_x, temp_y, temp_y_pos, temp_z = open_file(
+                evaluation, file, new_folder, temp_x, y, temp_y, temp_y_pos,
+                temp_z
+            )
 
         x_data.append(np.median(temp_x, axis=0))
         y_data.append(np.median(temp_y, axis=0))
@@ -72,12 +79,13 @@ def open_file(evaluation, filename, folder, x_data, y, y_data, y_pos, z_labels):
 
             for measure in data:
                 if (evaluation in measure) and measure.endswith(y):
-                   data = data[measure]
+                    data = data[measure]
 
             y_data.append(data)
             x_data.append(list(range(1, len(data)+1)))
 
     return (x_data, y_data, y_pos, z_labels)
+
 
 def calc_cumulative(y_data):
     """
@@ -99,7 +107,7 @@ if __name__ == "__main__":
     parser.add_argument("-f", "--folder_name", help="name of folder with data")
     parser.add_argument("-m", "--measure", help="offline or online evaluation")
     parser.add_argument("-x", "--x_label", help="label for x-axis")
-    parser.add_argument("-y", "--y_label", help="label for y-axis" \
+    parser.add_argument("-y", "--y_label", help="label for y-axis"
                         "(HAS TO BE EVALUATION MEASURE LIKE IN CONFIG)")
     parser.add_argument("-max_x", "--max_bound_x",
                         help="maximum number for x-axis", type=int)
@@ -116,5 +124,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     visualize_yaml(args.measure, args.folder_name, args.x_label, args.y_label,
-        args.max_bound_x, args.min_bound_y, args.max_bound_y, args.title,
-        args.run_mean)
+                   args.max_bound_x, args.min_bound_y, args.max_bound_y,
+                   args.title, args.run_mean)
