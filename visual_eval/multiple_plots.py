4cd86faad6be23328eda85e23b5559832dfd3523
Martin van Harmelen <Martin@vanharmelen.com>
2016-06-22 11:39:12 +0200

    Added major axis dashes lines

--- visual_eval/multiple_plots.py
+++ visual_eval/multiple_plots.py
@@ -1,15 +1,10 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
 
-def multiple_plots(x_data, y_data, y_pos, max_bound, ymin, ymax, title,
-                   x_label, y_label, z_label):
-    """
-    Plot data of multiple files in one graph.
-    """
-
-    # 20 RGB colours
-    colours = [
+# 20 RGB colours
+COLOURS = [
+    (r / 255., g / 255., b / 255.) for r, g, b in
+    [
         (31, 119, 180),  # (174, 199, 232),
         (255, 127, 14),  # (255, 187, 120),
         (44, 160, 44),  # (152, 223, 138),
@@ -21,11 +16,14 @@ def multiple_plots(x_data, y_data, y_pos, max_bound, ymin, ymax, title,
         (188, 189, 34),  # (219, 219, 141),
         (23, 190, 207),  # (158, 218, 229)
     ]
+]
 
-    # Scale the RGB values to the [0, 1] range
-    for i in range(len(colours)):
-        r, g, b = colours[i]
-        colours[i] = (r / 255., g / 255., b / 255.)
+
+def multiple_plots(x_data, y_data, y_pos, max_bound, ymin, ymax, title,
+                   x_label, y_label, z_label):
+    """
+    Plot data of multiple files in one graph.
+    """
 
     # Set figure size
     plt.figure(figsize=(15, 10))
@@ -45,18 +43,26 @@ def multiple_plots(x_data, y_data, y_pos, max_bound, ymin, ymax, title,
     plt.tick_params(axis="both", which="both", bottom="on", top="off",
                     labelbottom="on", left="on", right="off", labelleft="on")
 
-    # Add hardly visible lines at every +0.1
-    for y in np.arange(0, 1, 0.1):
-        plt.plot(range(0, max_bound), [y] * len(range(0, max_bound)),
-                 "--", lw=0.5, color="black", alpha=0.5)
+    # # Add hardly visible lines at every +0.1
+    # for y in np.arange(0, 1, 0.1):
+    #     plt.plot(range(0, max_bound), [y] * len(range(0, max_bound)),
+    #              "--", lw=0.5, color="black", alpha=0.5)
+    ax.grid(
+        which='major',
+        axis='y',
+        linestyle='--',
+        linewidth=0.5,
+        color='black',
+        alpha=0.5
+    )
 
     # Plot all data
     for i in range(len(x_data)):
-        plt.plot(x_data[i], y_data[i], lw=2, color=colours[i])
+        plt.plot(x_data[i], y_data[i], lw=2, color=COLOURS[i])
 
         # Add a text label to the right end of every line
         plt.text(max_bound + 5, y_pos[i], z_label[i],
-                 fontsize=17, color=colours[i])
+                 fontsize=17, color=COLOURS[i])
 
     # Add title to plot and labels to axes
     plt.xlabel(x_label, size=17)
