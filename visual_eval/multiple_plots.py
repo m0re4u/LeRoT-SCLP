c489151c2a8955d31ed70739f90a8ce2bb40421e
Martin van Harmelen <Martin@vanharmelen.com>
2016-06-22 13:30:15 +0200

    Fixed label things

--- visual_eval/multiple_plots.py
+++ visual_eval/multiple_plots.py
@@ -19,6 +19,14 @@ COLOURS = [
 ]
 
 
+def overlap(positions, size):
+    positions = sorted(positions)
+    for pos, next_pos in zip(positions, positions[1:]):
+        if pos + size > next_pos:
+            return True
+    return False
+
+
 def multiple_plots(x_data, y_data, y_pos, max_bound, ymin, ymax, title,
                    x_label, y_label, z_label):
     """
@@ -54,14 +62,43 @@ def multiple_plots(x_data, y_data, y_pos, max_bound, ymin, ymax, title,
         alpha=0.5
     )
 
-    # Plot all data
-    for x_datum, y_datum, color, y, z in zip(x_data, y_data, COLOURS, y_pos,
-                                             z_label):
-        plt.plot(x_datum, y_datum, lw=2, color=color)
+    # Plot all data and apply magic to the labels.
+    # I apologise for the following code.
+    MAX_LABELS, FONTSIZE, PADDING = 25, 17, 4  # Empirically tested (I'm sorry)
+    labelsize = (ymax - ymin) / float(MAX_LABELS)
+    paddingsize = labelsize / FONTSIZE * PADDING
+    labels = []
+    for x_datum, y_datum, color, y, label in zip(x_data, y_data, COLOURS,
+                                                 y_pos, z_label):
+        plt.plot(x_datum, y_datum, lw=2, color=color, label=label)
 
-        # Add a text label to the right end of every line
-        plt.text(max_bound + 5, y, z,
-                 fontsize=17, color=color)
+        # Add a text label to the right end of every line and centre it
+        labels.append({
+            'label': label,
+            'position': y - labelsize / 2. + paddingsize,
+            'color': color
+        })
+
+    labels = sorted(labels, key=lambda a: a['position'])
+
+    while overlap([l['position'] for l in labels], labelsize):
+        # Calculate the "force" on every label (in distance to be moved)
+        forces = [0.0 for _ in labels]
+        for this_i, next_pos in enumerate(l['position'] for l in labels[1:]):
+            this_pos = labels[this_i]['position']
+            force = (labelsize + this_pos - next_pos) / 2.
+            if force > 0:
+                forces[this_i] -= force
+                forces[this_i + 1] += force
+
+        # Apply forces
+        for i, force in enumerate(forces):
+            labels[i]['position'] += force
+
+    # Plot labels
+    for label in labels:
+        plt.text(max_bound + 10, label['position'], label['label'],
+                 fontsize=FONTSIZE, color=label['color'])
 
     # Add labels to axes
     plt.xlabel(x_label, size=26)
