80b128735a008378862ab447a17033dc88c2a12d
Martin van Harmelen <Martin@vanharmelen.com>
2016-06-23 10:29:59 +0200

    made create_histogram python3 compatible

--- visual_eval/create_histogram.py
+++ visual_eval/create_histogram.py
@@ -7,66 +7,79 @@ import yaml
 import sys
 
 SEAGREEN = (0, 128 / 255., 102 / 255.)
+INTERVAL = 500
+
 
 def create_histogram(filename, iterations, stepsize, x_label, y_label, max_x,
                      min_y, max_y, title):
-
     fig = plt.figure(facecolor='white')
 
-    datafile = open(filename,"r").read()
-    y_data = [sorted(data, reverse = True) for data in yaml.load(datafile)]
-    y_data = [y_data[i*stepsize] for i in range(0,len(y_data)/stepsize)]
+    datafile = open(filename, "r").read()
+    y_data = [sorted(data, reverse=True) for data in yaml.load(datafile)]
+    y_data = [y_data[i*stepsize] for i in range(0, len(y_data) // stepsize)]
     x_data = list(range(0, len(y_data[0])))
 
-    ani = animation.FuncAnimation(
+    fargs = [
+        x_data,
+        y_data,
+        fig,
+        min_y,
+        max_y,
+        x_label,
+        y_label,
+        title,
+        iterations
+    ]
+    animation.FuncAnimation(
         fig, animate,
-        fargs=[x_data, y_data, fig, min_y, max_y, x_label, y_label, title, iterations],
-        interval=100
+        fargs=fargs,
+        interval=INTERVAL
     )
 
     plt.show()
 
 
-def animate(i, x_data, y_data, fig, min_y, max_y, x_label, y_label, title,
+def animate(i, x_data, y_data, fig, min_y, max_y, x_label, y_label,
             iterations):
 
     if i > iterations:
         sleep(3)
         sys.exit()
-    else:
+    if i == 1:
+        sleep(5)
+    # Reset figure
+    fig.clear()
 
-        # Reset figure
-        fig.clear()
+    # Set text for figure and labels
+    plt.xlabel(x_label, size=26)
+    plt.ylabel(y_label, size=26)
+    plt.title("Iteration" + " " + str(i+1), size=26)
 
-        # Set text for figure and labels
-        plt.xlabel(x_label, size=26)
-        plt.ylabel(y_label, size=26)
-        plt.title("Iteration" + " " + str(i+1), size=26)
+    # Set axes sizes
+    max_x = len(x_data)
+    plt.ylim(min_y, max_y)
+    plt.xlim(0, max_x)
 
-        # Set axes sizes
-        max_x = len(x_data)
-        plt.ylim(min_y, max_y)
-        plt.xlim(0,max_x)
+    plt.bar(x_data, y_data[i], color=SEAGREEN)
 
-        plt.bar(x_data, y_data[i], color=SEAGREEN)
+    # Set visibility of plot frame lines
+    ax = plt.axes()
+    ax.spines["top"].set_visible(False)
+    ax.spines["bottom"].set_visible(True)
+    ax.spines["right"].set_visible(False)
+    ax.spines["left"].set_visible(True)
 
-        # Set visibility of plot frame lines
-        ax = plt.subplot(111)
-        ax.spines["top"].set_visible(False)
-        ax.spines["bottom"].set_visible(True)
-        ax.spines["right"].set_visible(False)
-        ax.spines["left"].set_visible(True)
+    # Remove the tick marks at top and right
+    plt.tick_params(axis="both", which="both", bottom="on", top="off",
+                    labelbottom="on", left="on", right="off",
+                    labelleft="on")
 
-        # Remove the tick marks at top and right
-        plt.tick_params(axis="both", which="both", bottom="on", top="off",
-                        labelbottom="on", left="on", right="off", labelleft="on")
+    # Add hardly visible lines at every +0.1
+    for y in np.arange(0, 1, 0.1):
+        plt.plot(range(0, max_x+1), [y] * len(range(0, max_x+1)),
+                 "--", lw=0.5, color="black", alpha=0.5)
 
-        # Add hardly visible lines at every +0.1
-        for y in np.arange(0, 1, 0.1):
-            plt.plot(range(0, max_x+1), [y] * len(range(0, max_x+1)),
-                     "--", lw=0.5, color="black", alpha=0.5)
-
-        return plt
+    return plt
 
 
 if __name__ == "__main__":
@@ -77,8 +90,8 @@ if __name__ == "__main__":
     parser.add_argument("-f", "--filename", help="name of file with data")
     parser.add_argument("-s", "--stepsize", help="stepsize for the animation",
                         type=int)
-    parser.add_argument("-i", "--iterations", help="number of iterations shown",
-                        type=int)
+    parser.add_argument("-i", "--iterations", type=int,
+                        help="number of iterations shown")
     parser.add_argument("-x", "--x_label", help="label for x-axis")
     parser.add_argument("-y", "--y_label", help="label for y-axis"
                         "(HAS TO BE EVALUATION MEASURE LIKE IN CONFIG)")
@@ -88,10 +101,9 @@ if __name__ == "__main__":
                         help="maximum number for y-axis", type=float)
     parser.add_argument("-min_y", "--min_bound_y",
                         help="minimum number for y-axis", type=float)
-    parser.add_argument("-title", help="title for image")
 
     args = parser.parse_args()
 
     create_histogram(args.filename, args.iterations, args.stepsize,
                      args.x_label, args.y_label, args.max_bound_x,
-                     args.min_bound_y, args.max_bound_y, args.title)
+                     args.min_bound_y, args.max_bound_y)
